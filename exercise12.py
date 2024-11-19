fc979e5f158aa2c6dfd7c9ec2c3f9c3d1492c03b
Tang Juyin <834578109@qq.com>
2024-11-19 23:43:51 +0200

    excise12

--- exercise12.py
+++ exercise12.py
@@ -1,3 +1,4 @@
+#1
 import json
 import requests
 request = "https://api.chucknorris.io/jokes/random"
@@ -13,3 +14,34 @@ try:
         print(f"Error: Received status code {response.status_code}")
 except requests.exceptions.RequestException as e:
     print ("Request could not be completed.")
+
+#2
+import requests
+def print_report(city):
+    coordinates = return_city_name(city)
+    lat = coordinates[0]
+    lon = coordinates[1]
+    request = (f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=82de97288a8db05cffb3f0e3a74a7d44')
+    response = requests.get(request).json()
+    weather = response['weather'][0]['main']
+    temperature = response['main']['temp']
+    print(f'The weather of {city.title()} is {weather} and the temperature is {temperature} °C.')
+
+def return_city_name(city_name):
+    name = []
+    request = (f"http://api.openweathermap.org/geo/1.0/direct?q={city_name.title()}&appid=82de97288a8db05cffb3f0e3a74a7d44")
+    response = requests.get(request).json()
+    name.append(response[0]['lat'])
+    name.append(response[0]['lon'])
+    return name
+
+def main():
+    try:
+        city_name = input('Enter city name: ')
+        print_report(city_name)
+    except IndexError:
+        print("Invalid city name, please try again.")
+        main()
+
+if __name__ == "__main__":
+    main()
